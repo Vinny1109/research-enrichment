64ae6dbfc1b39402a2e76ee7717f1d869ee406a4
Vinny1109 <104118028+Vinny1109@users.noreply.github.com>
2024-05-28 12:12:18 +0700

    Update inpainting-laplacian.py
    Memperbaharui eksekusi code dari 1 folder

--- inpainting-laplacian/inpainting-laplacian.py
+++ inpainting-laplacian/inpainting-laplacian.py
@@ -1,4 +1,11 @@
+import matplotlib.pyplot as plt
+from skimage import color #converting image to grayscale
+from skimage import io #reading writing image
+import glob
+import os
+import time
 import numpy as np #array-matrices
+np.random.seed(1)
 
 # Computes the relative squared error between two arrays.
 # antara gambar asli dengan gambar yg di inpaint
@@ -54,55 +61,148 @@ def laplacian_conv_2d(y_true, y, lmbda, gamma, eta, tau, maxiter = 50):
     del y_true, y
     show_iter = 10
     for it in range(maxiter):
+        # print("iteration ke", it)
+        # print("show iter: ", show_iter)
+        # print("iteration lap2d")
+        # print("\n")
         x = prox_2d(z, w, lmbda, denominator)
         z = update_z(y_train, pos_train, x, w, lmbda, eta)
         w = update_w(x, z, w, lmbda)
+        
         if (it + 1) % show_iter == 0:
             print(it + 1)
             print(compute_rse(y_test, x[pos_test]))
             print()
+            # io.imshow(z)
+            # plt.axis('off')
+            # plt.show()
+
     return x
 
-# masking 90% pixels as missing values
-import numpy as np
-np.random.seed(1)
+def number_file(path):
+    # mengambil nama file + type + extension
+    filename = os.path.basename(path) 
+    print("filename ",filename)
+    # hasilnya p1-nomor1_ori.png
+    
+    # mengambil nama file
+    filenumber = filename.rsplit('_', 1)[0]
+    print("filenumber",filenumber) 
+    # hasilnya p1-nomor1
+    
+    return filenumber
 
-import matplotlib.pyplot as plt
-from skimage import color #converting image to grayscale
-from skimage import io #reading writing image
-img = io.imread(r'C:\Users\vinny\OneDrive - Bina Nusantara\Documents\Research enrichment\inpainting\laplacian\test3.png') #png
-io.imshow(img)
-plt.show()
+def base_path(path):
+    # mundur 1 dir, c:\....\laplacian\defect
+    filebilap =  os.path.dirname(path)   
+    print("filebilap:", filebilap) 
+    
+    # mundur 1 dir, c:\....\laplacian
+    filebilap = os.path.dirname(filebilap)
+    print("filebilap:", filebilap) 
 
-# convert image to grayscale
-if img.shape[2] == 4:
-    img = img[:, :, :3]    
-imgGray = color.rgb2gray(img)
+    return filebilap
 
-# create the masked image
-M, N = imgGray.shape
-missing_rate = 0.9
-sparse_img = imgGray * np.round(np.random.rand(M, N) + 0.5 - missing_rate)
+def process_file(path):
+    start_time = time.time()
+    img = io.imread(path)
+    
+    # convert image to grayscale
+    if img.shape[2] == 4:
+        img = img[:, :, :3]    
+    imgGray = color.rgb2gray(img)
+    print("img shape: ", img.shape)
+   
+    M, N = imgGray.shape
+    missing_rate = 0.9
+    mask = imgGray * np.round(np.random.rand(M, N)+ 0.5 - missing_rate)
 
-io.imshow(sparse_img, cmap='gray')
-plt.axis('off')
-plt.show()
-# supaya bs save
-sparse_img_uint8 = (sparse_img * 255).astype(np.uint8)
-io.imsave(r'C:\Users\vinny\OneDrive - Bina Nusantara\Documents\Research enrichment\inpainting\laplacian\test3_masked_image.png', sparse_img_uint8)
+    print("sparse shape: ", mask.shape)
+    # io.imshow(mask, cmap='gray')
+    # plt.axis('off')
+    # plt.show()
+    filemask = os.path.join(base_path(path), 'mask', number_file(path) + '_mask.png')
+    print("filemask:", filemask)
+    
+    # supaya bs save
+    sparse_img_uint8 = (mask * 255).astype(np.uint8)
+    io.imsave(filemask, sparse_img_uint8)
+    end_time = time.time()
+    processing_time_mask = end_time - start_time
+    print("Waktu pemrosesan mask:", processing_time_mask, "detik")
+ 
+    start_time = time.time()
+    # proses inpainting
+    lmbda = 5e-3 * M * N
+    gamma = 1 * lmbda
+    eta = 100 * lmbda
+    tau = 2
+    maxiter = 100
+    mat_hat = laplacian_conv_2d(imgGray, mask, lmbda, gamma, eta, tau, maxiter)
+    
+    mat_hat[mat_hat < 0] = 0
+    mat_hat[mat_hat > 1] = 1
+    # io.imshow(mat_hat)
+    # plt.axis('off')
+    fileinp = os.path.join(base_path(path), 'inp', number_file(path) + '_inp.png')
+    print("fileinp:", fileinp)
+    plt.imsave(fileinp.format(tau), mat_hat, cmap = plt.cm.gray)
+    end_time = time.time()
+    processing_time_inpaint = end_time - start_time
+    print("Waktu pemrosesan inpaint:", processing_time_inpaint, "detik")
 
-# proses inpainting
-lmbda = 5e-3 * M * N
-gamma = 1 * lmbda
-eta = 100 * lmbda
-tau = 2
-maxiter = 100
-mat_hat = laplacian_conv_2d(imgGray, sparse_img, lmbda, gamma, eta, tau, maxiter)
+    #bikin plot
+    fig, axes = plt.subplots(ncols=2, nrows=2)
+    ax = axes.ravel()
 
-mat_hat[mat_hat < 0] = 0
-mat_hat[mat_hat > 1] = 1
-io.imshow(mat_hat)
-plt.axis('off')
-plt.imsave(r'C:\Users\vinny\OneDrive - Bina Nusantara\Documents\Research enrichment\inpainting\laplacian\test3_gray_recovery.png'.format(tau), 
-           mat_hat, cmap = plt.cm.gray)
-plt.show()
+    ax[0].set_title('Original image')
+    fileori = os.path.join(base_path(path), 'ori', number_file(path) + '_ori.png')
+    print("fileori:", fileori) 
+    ax[0].imshow(io.imread(fileori))
+
+    ax[1].set_title('Mask')
+    ax[1].imshow(mask, cmap=plt.cm.gray)
+
+    ax[2].set_title('Defected image')
+    ax[2].imshow(img)
+
+    ax[3].set_title('Inpainted image')
+    ax[3].imshow(io.imread(fileinp))
+    
+    for a in ax:
+        a.axis('off')
+
+    fig.tight_layout()
+    plt.show()
+
+def main():
+    file_onepath = r'C:\Users\vinny\OneDrive - Bina Nusantara\Documents\Research-enrichment\inpainting\laplacian\defect\p1-nomor50_defect.png'
+    folder_path = 'C:\\Users\\vinny\\OneDrive - Bina Nusantara\\Documents\\Research-enrichment\\inpainting\\laplacian\\defect'
+    
+    # comment yang tidak digunakan
+    file_use = folder_path 
+    # file_use = file_onepath 
+    n = 1
+ 
+    if os.path.isfile(file_use):
+        print("file")
+        process_file(file_use)
+    elif os.path.isdir(file_use):
+        file_type = '*.png'  
+        # Menggunakan glob untuk mencocokkan pola file
+        file_list = glob.glob(os.path.join(folder_path, file_type))
+
+        # Iterasi semua file yang cocok
+        for file_path in file_list:
+            print("file pathdisinii: ", file_path)
+            absolute_path = os.path.abspath(file_path)
+            print("file pathdisinii: ", file_path)
+            process_file(absolute_path)
+            print("=====================")
+            print("process file ke: ", n)
+            print("=====================")
+            n = n + 1
+            
+
+if __name__ == "__main__":
+    main()
